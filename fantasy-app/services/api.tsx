f56ebe21b32fbb510fecc272c8dc0920a654ef61
jmcook_21 <jmcook5520@gmail.com>
2026-05-11 18:11:22 -0400

    localhost removal

--- fantasy-app/services/api.tsx
+++ fantasy-app/services/api.tsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 
-export const URL = "http://localhost:5001"; // replace with your IP
+export const URL = process.env.EXPO_PUBLIC_API_URL; // replace with your IP
 
 const API = axios.create({
   baseURL:
