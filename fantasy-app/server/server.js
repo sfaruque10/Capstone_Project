6142adab92ab93967ff94d2aa873a23f976e3076
jmcook_21 <jmcook5520@gmail.com>
2026-05-11 17:34:22 -0400

    Pre deployment fixes so local running is possible

--- fantasy-app/server/server.js
+++ fantasy-app/server/server.js
@@ -77,7 +77,7 @@ cron.schedule("0 3 * * *", async () => {
 
 const PORT = process.env.PORT || 5001;
 
-app.listen(PORT, () => {
+app.listen(PORT, "0.0.0.0", () => {
   console.log(
     `Server running on port ${PORT}`,
   );
