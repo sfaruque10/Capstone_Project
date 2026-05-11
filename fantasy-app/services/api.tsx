c2f90c98dc2a545d49c38c77e2e929938b78779a
sfaruque10 <98576322+sfaruque10@users.noreply.github.com>
2026-05-11 17:04:30 -0400

    Update api.tsx

--- fantasy-app/services/api.tsx
+++ fantasy-app/services/api.tsx
@@ -3,7 +3,6 @@ import AsyncStorage from "@react-native-async-storage/async-storage";
 
 export const URL = "http://localhost:5001"; // replace with your IP
 
-// export const URL = "http://192.168.1.139:5001";
 const API = axios.create({
   baseURL: URL,
 });
